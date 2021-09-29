599bc11c722080de668b1cb8c8641442907a312e
Harrison Burt <hburt2003@gmail.com>
2021-09-29 14:49:34 +0100

    make cli parser

---
+++ EmailExtractor/CommandParser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace EmailExtractor
+{
+    public class CommandParser
+    {
+        private readonly Regex _regex;
+        private readonly Dictionary<string, string?> _values;
+
+        public CommandParser()
+        {
+            _regex = new Regex(@"(?<flag>--[\w\-]+)(?: (?<value>[\w\-]+))?", RegexOptions.Compiled);
+            _values = new Dictionary<string, string?>();
+        }
+
+        public bool Exists(string flag)
+        {
+            return _values.ContainsKey(flag);
+        }
+
+        public string? GetFlagValue(string flag)
+        {
+            return Exists(flag) ? _values[flag] : null;
+        }
+
+        public void ParseArgs(string[] args)
+        {
+            var cliArgs = string.Concat(args);
+            var matches = _regex.Matches(cliArgs);
+            foreach (Match match in matches)
+            {
+                var flag = match.Groups["flag"].Value;
+
+                var valueGroup = match.Groups["value"];
+                var value = valueGroup.Success ? valueGroup.Value : null;
+
+                _values[flag] = value;
+            }
+        }
+    }
+}
--- EmailExtractor/Program.cs
+++ EmailExtractor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 
 namespace EmailExtractor
@@ -9,10 +10,15 @@ namespace EmailExtractor
     {
         static void Main(string[] args)
         {
-            var noTld = args.Contains("--notld");
-            var caseInsensitive = args.Contains("--caseInsensitive");
+            var commands = new CommandParser();
+            commands.ParseArgs(args);
             
-            var data = GetData();
+            var noTld = commands.Exists("--notld");
+            var caseInsensitive = commands.Exists("--caseInsensitive");
+            var httpUrl = commands.GetFlagValue("--url");
+
+            var data = httpUrl is null ? GetData() : GetUrlData(httpUrl);
+            
             var regex = new Regex(
                 @"(?:[a-z0-9]+[\.\-_]?[a-z0-9]+)+@(?<domain>[a-z0-9\-]+)(?<tld>(\.[a-z]{2,})+)", 
                 RegexOptions.Compiled | RegexOptions.IgnoreCase
@@ -37,15 +43,20 @@ namespace EmailExtractor
                 counter.Register(domain);
             }
             
-            counter.Display();
+            counter.Display(keyColumnName: "Domain");
             Console.WriteLine("");
-            counter.DisplayAppearingDomains(89);
+            counter.DisplayAppearingDomains(89, "Domain");
         }
 
+        static string GetUrlData(string url)
+        {
+            var client = new WebClient();
+            return client.DownloadString(url);
+        }
+        
         static string GetData()
         {
             return File.ReadAllText("./data/sample.txt");
-            
         }
     }
 }
