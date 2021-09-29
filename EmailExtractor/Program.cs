91a58c50ebfa1bd6eee6c5e6b62f1465e0e83775
Harrison Burt <hburt2003@gmail.com>
2021-09-29 10:56:48 +0100

    Add sample.txt and base program

--- EmailExtractor/Program.cs
+++ EmailExtractor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace EmailExtractor
 {
@@ -7,6 +9,27 @@ namespace EmailExtractor
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var data = GetData();
+            var regex = new Regex(@"",  RegexOptions.Compiled);
+
+            var counter = 0;
+            for (var i = 0; i < (data.Length - 13); i++)
+            {
+                if (data.Substring(i, 13) == "@softwire.com")
+                {
+                    counter += 1;
+                } 
+            }
+            
+            Console.WriteLine(counter);
+        }
+        
+        
+
+        static string GetData()
+        {
+            return File.ReadAllText("./data/sample.txt");
+            
         }
     }
 }
